bfa2c47aab79084671c0fc4bfd2cd6d116797a6c
Bhaskar <shivamti1536@gmail.com>
2024-08-08 16:08:47 +0530

    final commit

--- db/server.js
+++ db/server.js
@@ -1,16 +1,16 @@
 const mysql = require("mysql");
 
 const con = mysql.createConnection({
-  // host: "localhost",
-  // user: "fnnodeapi", // Replace with your MySQL username
-  // password: "Km)sP$_VKz-P", // Replace with your MySQL password
-  // database: "fnnodeapi",
-  // port: 3306,
-  user: "root",
-  host: "localhost",
-  password: "",
-  database: "sachinproject",
-  port: 3308,
+  host: "sql5.freesqldatabase.com",
+  user: "sql5724705", // Replace with your MySQL username
+  password: "fwZK9px9WA", // Replace with your MySQL password
+  database: "sql5724705",
+  port: 3306,
+  //   user: "root",
+  //   host: "localhost",
+  //   password: "",
+  //   database: "sachinproject",
+  //   port: 3308,
 });
 
 function connectionDb() {
